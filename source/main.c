e8d323836f0877b23484c41cab8bfe5c48a016bd
Katelyn Vi <kvi001@wch136-23.cs.ucr.edu>
2020-01-14 00:57:14 -0800

    save changes

--- source/main.c
+++ source/main.c
@@ -16,14 +16,38 @@ int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
-    	unsigned char tmp1 = 0x00;
+	unsigned char tmp1 = 0x00;
 	unsigned char tmp2 = 0x00;
+	unsigned char cntavail = 0x04;
+
     /* Insert your solution below */
     while (1) {
+	// 2.2 answer
+	tmp1 = PINA & 0x0F; // check if PINA is 0000 1111 
+	if (tmp1 & 0x01) {
+		cntavail = cntavail - 1;
+	} 		
+
+	if (tmp1 & 0x02) { // checks if tmp1(PINA) is 0010
+                cntavail = cntavail - 1;
+        }
+
+	if (tmp1 & 0x04) {  // checks if tmp1(PINA) is 0100
+                cntavail = cntavail - 1;
+        }
+
+	if (tmp1 & 0x08) {  // checks if tmp1(PINA) is 1000
+                cntavail = cntavail - 1;
+        }
+	
+	PORTB = cntavail & 0x0F; //assign PORTC to num cntavail
+	/*
+	// 2.1 answer
 	tmp1 = PINA & 0x01; // see if PINA is 0x01
 	tmp2 = PINA & 0x02; // see if PINA is 0x02
 	PORTB = ((!tmp2) & tmp1); // assigns PORTB with 1 if PINA1 = 0 and PINA0 = 1 
-	/*
+	
+	// first attemp 2.1
 	tmpA = PINA & 0x03;
 	if (tmpA == 0x01) {
 		PORTB = 0x01;
